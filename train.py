854a04dcf4b544c00b3367feea6eee424a10d8d0
Guohao Dou <gdou2@illinois.edu>
2017-11-27 01:41:43 -0600

    i

--- train.py
+++ train.py
@@ -16,9 +16,9 @@ dummy_input = torch.unsqueeze(dummy_inputs[0], dim=0)
 dummy_targets = Variable(torch.rand((1000, 10)))
 
 # set parameters!
-learning_rate = 0.01
-epoch = 1
-batch_size = 5
+learning_rate = 0.001
+epoch = 2
+batch_size = 10
 
 # Register parameters
 numb_param({
