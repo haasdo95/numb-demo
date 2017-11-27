b5fc11f2f9d0f2dd3841267b0359f7f6d5f5b5a1
Guohao Dou <gdou2@illinois.edu>
2017-11-27 04:40:26 -0600

    t

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import sys
 sys.path.append('/Users/dwd/go/src/github.com/user/numb')
 
 from lenet import LeNet
-# changgggge
-model = LeNet() # change!
+# I am making change
+model = LeNet() # chaaaaange!
