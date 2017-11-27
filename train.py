38016dbed93ca2faa94163f52b1dac53328267d2
Guohao Dou <gdou2@illinois.edu>
2017-11-27 13:30:42 -0600

    t

--- train.py
+++ train.py
@@ -17,8 +17,8 @@ dummy_targets = Variable(torch.rand((1000, 10)))
 
 # set parameters!
 learning_rate = 0.01
-epoch = 1
-batch_size = 10 # becomes ten
+epoch = 2
+batch_size = 20 # becomes twenty
 
 # Register parameters
 numb_param({
