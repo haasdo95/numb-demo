bd727c49488976382d636a406bb3b88be8e46952
Guohao Dou <gdou2@illinois.edu>
2017-12-03 17:04:44 -0600

    x

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
 import sys
 sys.path.append('/Users/dwd/go/src/github.com/user/numb')
 
+from pyhooks.hooks import numb_test_result
+
 from lenet import LeNet
 # I am making change
 model = LeNet() # chaaaaange!
+
+numb_test_result({
+    "accuracy": 1,
+    "note": "trying out"
+})
--- train.py
+++ train.py
@@ -16,9 +16,9 @@ dummy_input = torch.unsqueeze(dummy_inputs[0], dim=0)
 dummy_targets = Variable(torch.rand((1000, 10)))
 
 # set parameters!
-learning_rate = 0.01
-epoch = 2
-batch_size = 10 # becomes ten
+learning_rate = 0.1
+epoch = 1
+batch_size = 20 # becomes ten
 
 # Register parameters
 numb_param({
